0e627ff1884c1409f6640a47d288effd4addc241
kazantsev Denis <denspa@mail.ru>
2020-07-20 23:31:00 +0600

    добовик ENUM

--- app/src/main/java/com/geekbrains/mvc/Presenter.java
+++ app/src/main/java/com/geekbrains/mvc/Presenter.java
@@ -1,6 +1,21 @@
 package com.geekbrains.mvc;
 
 public class Presenter {
+    public enum NumBt {
+
+        BT1(0),
+        BT2(1),
+        BT3(2);
+        int btId;
+
+        NumBt(int btId) {
+            this.btId = btId;
+        }
+
+        public int getBtId() {
+            return btId;
+        }
+    }
 
     private final Model mModel;
     private final MainView mView;
